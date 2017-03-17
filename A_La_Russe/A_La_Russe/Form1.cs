2fd338dc0ea2c7fbd6b7ccde043109ec4c2653c8
RobertPopaBv <poparobertbv@gmail.com>
2017-03-17 12:37:38 +0200

    Aplicatie A_La_Russe

---
+++ A_La_Russe/A_La_Russe/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A_La_Russe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+            int b;
+
+            a = int.Parse(textBox1.Text);
+            b = int.Parse(textBox2.Text);
+
+            int prod = 0;
+
+            while(a > 0)
+            {
+                if(a%2 == 1)
+                {
+                    prod = prod + b;
+                }
+                a = a / 2;
+                b = b + b;
+            }
+
+            MessageBox.Show(prod.ToString());
+
+
+        }
+
+    }
+}
